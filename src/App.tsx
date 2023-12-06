313deac5d7c11f58fd93cd57d1aeffaf3e1d2949
abhilashajayan2001 <abhilashajayan2001@gmail.com>
2023-12-06 08:28:22 +0530

    the design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const  App : React.FC=()=> {
+
+const[items,setItems] = useState<string>()
+
+
   return (
-    <div className='bg-navy-blue h-screen w-screen fixed'>
-      <div className='justify-center flex '>
-        <h1 className='text-blue-200 text-4xl p-5'>Todo App</h1>
+    <div className='bg-navy-blue h-screen w-screen fixed flex flex-col justify-center items-center'>
+       <div className='flex '>
+           <h1 className='text-blue-200 text-4xl p-5'>Todo App</h1>
+       </div>
+      <div className='flex items-center flex-col'>
+         <form>
+           <input type="text" className='w-60 bg-gray-400 h-10 border rounded-md p-2 ' />
+         </form>
+         <button className='px-4 mt-2 py-2 bg-electric-blue w-full  rounded-md'>ADD</button>
       </div>
-   
+
+      <div className='mt-3  max-w-[240px] w-full  h-[200px]  overflow-y-auto'>
+        <div className='py-2 text-blue-200 border  rounded flex'>
+            <p >hello</p>
+            <button className='ms-auto mr-2'>i</button>
+        </div>
+      </div>
     </div>
+
   )
 }
 
---
+++ src/Components/List.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Todoform: React.FC = () => {
+  return (
+    <div>Todoform</div>
+  )
+}
+
+export default Todoform
